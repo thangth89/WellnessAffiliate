4bf8bf489101875fad7dd7603e0fd7f6976cced6
thangth89 <thangth.vinfast@gmail.com>
2025-10-04 18:07:54 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,29 +58,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>  
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-         {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M5PSTT6W');
-        `}
-        </Script>     
-      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
-        
-         {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe 
-              src="https://www.googletagmanager.com/ns.html?id=GTM-M5PSTT6W"
-              height="0" width="0" style={{display:"none",visibility:"hidden"}}>
-            </iframe>
-          </noscript>
-     
-            
         <div className="fixed top-0 left-0 right-0 z-50">
           <Analytics/>
           <TailwindScrollingBanner />
